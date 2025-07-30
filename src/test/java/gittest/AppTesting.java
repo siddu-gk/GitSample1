349529439dafbd8082a1526bd8ad9083765f2592
siddu <sidduggs100@gmail.com>
2025-07-30 21:41:16 +0530

    new commit by user x in esteren time

---
+++ src/test/java/gittest/AppTesting.java
@@ -0,0 +1,13 @@
+package gittest;
+
+import org.testng.annotations.Test;
+
+public class AppTesting {
+
+	@Test
+	public void testing()
+	{
+		System.out.println(" Test 1 completed");
+		System.out.println(" Test 2 completed");
+	}
+}
