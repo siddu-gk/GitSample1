4272f4d730c07f697d8e4ed6820b1e28c113c7ff
siddu <sidduggs100@gmail.com>
2025-07-31 20:11:14 +0530

    Architect commit1 in EST time zone

---
+++ src/test/java/gitdevelop/AppDeveloping.java
@@ -0,0 +1,31 @@
+package gitdevelop;
+
+import org.testng.annotations.Test;
+
+public class AppDeveloping {
+
+	@Test
+	public void loginPageDeveloping()
+	{
+		System.out.println(" dev 1 completed");
+		System.out.println(" dev 2 completed");
+		System.out.println(" dev 3 completed");
+		System.out.println(" dev 4 completed");
+	}
+	
+	@Test
+	public void featureDeveloping()
+	{
+		System.out.println("feature 1 developing is completed");
+		System.out.println("feature 2 developing is completed");
+		System.out.println("feature 3 developing is completed");
+	}
+	
+	@Test
+	public void uiDeveloping()
+	{
+		System.out.println("ui 1 developing is completed");
+		System.out.println("ui 2 developing is completed");
+		System.out.println("ui 3 developing is completed");
+	}
+}
--- src/test/java/gittest/AppTesting.java
+++ src/test/java/gittest/AppTesting.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class AppTesting {
 
 	@Test
-	public void testing()
+	public void loginTesting()
 	{
 		System.out.println(" Test 1 completed");
 		System.out.println(" Test 2 completed");
@@ -14,9 +14,18 @@ public class AppTesting {
 	}
 	
 	@Test
-	public void developing()
+	public void featureTesting()
 	{
-		System.out.println("dev 1 is completed");
-		System.out.println("dev 2 is completed");
+		System.out.println("feature 1 is testing is completed");
+		System.out.println("feature 2 is testing is completed");
+		System.out.println("feature 3 is testing is completed");
+	}
+	
+	@Test
+	public void uiTesting()
+	{
+		System.out.println("ui 1 testing is completed");
+		System.out.println("ui 2 testing is completed");
+		System.out.println("ui 3 testing is completed");
 	}
 }
